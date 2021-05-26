1733c58c24444bbc6cbb7bd072411dbe8dd4f4d7
Phonexai720 <78328451+Phonexai720@users.noreply.github.com>
2021-05-26 17:59:52 +0700

    Add files via upload

---
+++ bai3 th7.py
@@ -0,0 +1,7 @@
+text_file =open("filename.dat","r")
+
+lines =text_file.reaadlines()
+
+print lines
+
+print len(lines)
---
+++ bai4 th7.py
@@ -0,0 +1,9 @@
+def file_read_head(fname, nlines):
+
+    from itertools import islice
+
+    with open(fname)as f:
+
+        for line in islice(f, nlines):
+
+            print(line)
